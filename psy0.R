7164175fb5ee786974a5393acd56a028a682b953
Kpragados <142109895+Kpragados@users.noreply.github.com>
2024-07-17 13:56:18 -0400

    Add files via upload

---
+++ psy0.R
@@ -0,0 +1,16 @@
+#Askes the user for two numbers
+x = float(input("Enter a number"))
+y = float(input("Enter another number"))
+
+
+# Multiples the first number to the power of the second number
+sum = x ** y
+
+
+#Prints the result
+print(sum)
+
+
+       
+
+
